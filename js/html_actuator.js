df70bd80e40e7faa4efa966c6f11260345436294
Josh Stone <cuviper@gmail.com>
2014-04-06 12:30:28 -0700

    Re-enable twitter sharing buttons, custom to ±64

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -156,13 +156,12 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-via", "CUViper");
+  tweet.setAttribute("data-url", "http://cuviper.github.io/pm64/");
   tweet.textContent = "Tweet";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
+  var text = "I scored " + this.score + " points at ±64, a #2048game " +
+             "variant with both positive and negative tiles! #pm64game";
   tweet.setAttribute("data-text", text);
 
   return tweet;
